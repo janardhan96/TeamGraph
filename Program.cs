1cfb68a686c68a9206cac0e8fa95c633cbd47566
janardhan <janardhanreddy2296@gmail.com>
2019-11-29 20:11:53 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace TeamsGraph
         {
             GraphClient graphClient = new GraphClient(new GraphClientConfig());
             var s = graphClient.GetTeams(new Connection() { UserName = "" });
-            s.Wait();
+            s.Wait(); 
 
         }
     }
